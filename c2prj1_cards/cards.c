1b11d432b5855cc4e1c7cb4e8d06571d3e8475f0
Coursera Learner <nobody@nowhere.nul>
2020-12-24 19:42:56 +0000

    Done

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -62,33 +62,56 @@ void print_card(card_t c) {
 
 card_t card_from_letters(char value_let, char suit_let) {
   card_t temp;
-  temp.value = value_let;
-  temp.suit =suit_let;
-  assert_card_valid(temp);
-  temp.value = value_letter(temp);
-  temp.suit = suit_letter(temp);
+  switch(value_let) {
+  case '2' : temp.value = 2; break;
+  case '3' : temp.value = 3; break;
+  case '4' : temp.value = 4; break;
+  case '5' : temp.value = 5; break;
+  case '6' : temp.value = 6; break;
+  case '7' : temp.value = 7; break;
+  case '8' : temp.value = 8; break;
+  case '9' : temp.value = 9; break;
+  case '0' : temp.value = 10; break;
+  case 'J' : temp.value = VALUE_JACK; break;
+  case 'Q' : temp.value = VALUE_QUEEN; break;
+  case 'K' : temp.value = VALUE_KING; break;
+  case 'A' : temp.value = VALUE_ACE; break;
+  default : temp.value = 0; }
+  switch(suit_let) {
+  case 's' : temp.suit = SPADES; break;
+  case 'h' : temp.suit = HEARTS; break;
+  case 'd' : temp.suit = DIAMONDS; break;
+  case 'c' : temp.suit = CLUBS; break;
+  default : temp.suit = 0; }
+ 
   return temp;
 }
+
 
 card_t card_from_num(unsigned c) {
   card_t temp;
 
   if (0 <= c && c < 52 )
     {
-      temp.suit = c/13+1;
+      
       if (0 <= c && c <= 12){
 	temp.value = c + 2;
+	temp.suit = SPADES;
       }
       if (12 < c && c <= 25){
 	temp.value = c - 11;
+	temp.suit = HEARTS;
       }
       if (25 < c && c <= 38){
 	temp.value = c - 24;
+	temp.suit = DIAMONDS;
       }
       if (38 < c && c < 52){
 	temp.value = c - 37;
+	temp.suit = CLUBS;
       }
     }
+ 
       return temp;
 }
   
--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -8,20 +8,28 @@ int main(void) {
     card_t c1;
     c1.value = 14;
     c1.suit = 2;
-    assert_card_valid(card_t c1)
+    assert_card_valid(c1);   
     print_card(c1);
 
     card_t c2;
     c2.value = 5;
     c2.suit = 1;
-    assert_card_valid(card_t c2)
     print_card(c2);
 
     card_t c3;
     c3.value = 11;
     c3.suit = 3;
-    assert_card_valid(card_t c3)
+   
     print_card(c3);
-    return EXIT_SUCCESS;
+
+    card_t c4 = card_from_num(51);
+    assert_card_valid(c4);
+    print_card(c4);
+
+    card_t c5 = card_from_letters('9','d');
+    print_card(c5);
+
+   
+       return EXIT_SUCCESS;
 }
     
