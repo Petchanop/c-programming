c8f67e329638859c2f1f7fd753219ab2d587b903
Coursera Learner <nobody@nowhere.nul>
2021-01-18 14:26:54 +0000

    Done

--- 22_tests_power/test-power.c
+++ 22_tests_power/test-power.c
@@ -7,7 +7,7 @@ int main(){
   if(power(1,0) != 1){return EXIT_FAILURE;} 
   if(power(-1,2) != 1){return EXIT_FAILURE;}
   if(power(0,0) != 0){return EXIT_FAILURE;}
-  if(power(2,33) != 8589934592){return EXIT_FAILURE;}
+  if(power(2,10) != 1024){return EXIT_FAILURE;}
   return EXIT_SUCCESS;
  
 }
