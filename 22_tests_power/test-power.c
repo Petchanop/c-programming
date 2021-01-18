fdeea1c3eb0be6721981901224fe3043a00dee27
Coursera Learner <nobody@nowhere.nul>
2021-01-18 14:40:29 +0000

    Done

--- 22_tests_power/test-power.c
+++ 22_tests_power/test-power.c
@@ -6,8 +6,8 @@ unsigned power (unsigned x, unsigned y);
 int main(){
   if(power(1,0) != 1){return EXIT_FAILURE;} 
   if(power(-1,2) != 1){return EXIT_FAILURE;}
-  if(power(0,0) != 0){return EXIT_FAILURE;}
-  if(power(2,10) != 1024){return EXIT_FAILURE;}
+  if(power(2,22) != 4194304){return EXIT_FAILURE;}
+  if(power(0,0) != 1){return EXIT_FAILURE;}
   return EXIT_SUCCESS;
  
 }
