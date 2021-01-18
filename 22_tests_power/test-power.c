199c216d8d805bff95985d1fea3e74c30bfe545d
Coursera Learner <nobody@nowhere.nul>
2021-01-18 10:14:07 +0000

    Done

--- 22_tests_power/test-power.c
+++ 22_tests_power/test-power.c
@@ -6,12 +6,12 @@ unsigned power (unsigned x, unsigned y);
 int main(){
   unsigned x1 = 1;
   unsigned y1 = 0;
-  unsigned x2 = 2;
-  unsigned y2 = 100;
+  unsigned x2 = -3;
+  unsigned y2 = 29;
   if(power(x1,y1) != 1){return EXIT_FAILURE;}
   if(power(y1,x1) != 0){return EXIT_FAILURE;}
   if(power(y1,y1) != 0){return EXIT_FAILURE;}
-  if(power(x2,x2) != 100){return EXIT_FAILURE;}
+  if(power(x2,y2) != -68630377364883){return EXIT_FAILURE;}
   else{
     return EXIT_SUCCESS;
   }
