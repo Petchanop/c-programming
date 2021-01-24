42d24c47d69f55758843a51c364f508fb2e6ab69
Coursera Learner <nobody@nowhere.nul>
2021-01-24 16:27:02 +0000

    Done

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -5,9 +5,9 @@
 
 void print_hand(deck_t * hand){
   card_t * p;
-   for (int i  = 0; i < hand->n_cards ; i++){
-     p = hand->cards[i]; 
-    print_card(* p);
+  for (int i  = 0; i <(*hand)->n_cards ; i++){
+    p = (*hand)->cards[i]; 
+    print_card(p);
      printf(" ");
   }
 }
