5e41bf09fecd67a399eebe0f9e0c5f478bb96172
Coursera Learner <nobody@nowhere.nul>
2021-01-24 16:43:35 +0000

    Done

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -4,12 +4,11 @@
 #include "deck.h"
 
 void print_hand(deck_t * hand){
-  card_t  p;
+  card_t ** p = hand->cards;
   for (int i  = 0; i < hand->n_cards ; i++){
-    p = hand->cards[i]; 
-    print_card(p);
-     printf(" ");
-  }
+    print_card(** p);
+    printf(" ");
+    p++; }
 }
 
 int deck_contains(deck_t * d, card_t c) {
