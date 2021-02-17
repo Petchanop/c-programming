16cd3638e50d2a2da26391eae949f25aa0c35523
Coursera Learner <nobody@nowhere.nul>
2021-02-17 20:44:41 +0000

    Done

---
+++ 25_break_encr/breaker.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int freqcount(FILE * f,int * count){
+  int c;
+  while ((c = fgetc(f)) != EOF) {
+    if (isalpha(c)) {
+      c = tolower(c);
+      c -= 'a';
+      count[c] += 1;
+    }
+  }
+  int max = 0;
+  int i = 0 ;
+    while (i < 26){
+    if (count[i] > max){
+      max = count[i];
+
+    }
+    i++;
+  }
+  return max;
+}
+
+void breaker(FILE * f){
+  int count[26] = {0};
+  int key = freqcount(f,count);
+  int de = 26 - (key + ("e" - "a")) ;
+  printf("%d\n",de);
+}
+
+int main(int argc, char ** argv) {
+  if (argc != 2) {
+    fprintf(stderr,"Usage: encrypt key inputFileName\n");
+    return EXIT_FAILURE;
+  }
+
+  FILE * f = fopen(argv[1], "r");
+  if (f == NULL) {
+    perror("Could not open file");
+    return EXIT_FAILURE;
+  }
+  breaker(f);
+  if (fclose(f) != 0) {
+    perror("Failed to close the input file!");
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
