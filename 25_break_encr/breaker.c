b533fb0e1b873b0a8b0e1dac9f416e1860a74e4c
Coursera Learner <nobody@nowhere.nul>
2021-02-17 21:58:17 +0000

    Done

--- 25_break_encr/breaker.c
+++ 25_break_encr/breaker.c
@@ -35,12 +35,8 @@ int breaker(FILE * f){
 }
 
 int main(int argc, char ** argv) {
-  if (argc != 2) {
-    fprintf(stderr,"Usage: encrypt key inputFileName\n");
-    return EXIT_FAILURE;
-  }
-
-  FILE * f = fopen(argv[1], "r");
+  if (argc == 2) {
+     FILE * f = fopen(argv[1], "r");
   if (f == NULL) {
     perror("Could not open file");
     return EXIT_FAILURE;
@@ -50,6 +46,7 @@ int main(int argc, char ** argv) {
   if (fclose(f) != 0) {
     perror("Failed to close the input file!");
     return EXIT_FAILURE;
+  }
   }
   return EXIT_SUCCESS;
 }
