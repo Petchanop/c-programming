edb2ac3fc3b59bba0f08129926296ce12756283f
Coursera Learner <nobody@nowhere.nul>
2021-02-25 17:40:00 +0000

    Done

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -63,16 +63,16 @@ int main(int argc, char ** argv) {
   if (argc > 1){
     for (int i = 1 ; i < argc ; i++){
       FILE * f = fopen(argv[i],"r");
+      if (f == NULL) {
+	perror("Could not open file");
+	return EXIT_FAILURE;
+      }
       int c = fgetc(f);
       if(c == EOF){
 	fprintf(stderr,"Usage: Empty File\n");
 	return EXIT_FAILURE;
       }
-      if (f == NULL) {
-	perror("Could not open file");
-	return EXIT_FAILURE;
-      }
-      printSort(f);
+     printSort(f);
      fclose(f);
     }
   }
