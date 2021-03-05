52e5c9e7e5e1bbf28282c19b9d84b58c334b44be
Coursera Learner <nobody@nowhere.nul>
2021-03-05 16:35:22 +0000

    Done

--- 34_put_together/main.c
+++ 34_put_together/main.c
@@ -40,7 +40,11 @@ counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
 
 int main(int argc, char ** argv) {
   //WRITE ME (plus add appropriate error checking!)
- //read the key/value pairs from the file named by argv[1] (call the result kv)
+  if (argc < 2){
+    perror("Not enought arguments");
+    exit(EXIT_FAILURE);
+  }
+  //read the key/value pairs from the file named by argv[1] (call the result kv)
   kvarray_t * kv = readKVs(argv[1]);
  //count from 2 to argc (call the number you count i)
   for (int i = 2 ; i < argc ; i++){
