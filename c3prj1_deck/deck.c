66c98d6ea1a5eef2dc3abfacec6a54212acf1e23
Coursera Learner <nobody@nowhere.nul>
2021-03-07 18:32:58 +0000

    Done

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -8,8 +8,8 @@ void add_card_to(deck_t * deck, card_t c){
   card_t * ptr = malloc(sizeof(*ptr));
 			ptr->value = c.value;
 			ptr->suit = c.suit;
-  deck->cards[deck->n_cards] = ptr;
-  
+			(deck->cards)[deck->n_cards] = ptr;
+     
 }
 
 card_t * add_empty_card(deck_t * deck){
