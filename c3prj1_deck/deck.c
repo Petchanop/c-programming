d9f9609243e9cffc7e99064bef3ba7032884be47
Coursera Learner <nobody@nowhere.nul>
2021-03-07 20:12:23 +0000

    Done

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -35,12 +35,12 @@ return ex;
 }
         
 deck_t * build_remaining_deck(deck_t ** hands, size_t n_hands){
-  deck_t * build = = malloc(sizeof(*ex));
+  deck_t * build = = malloc(sizeof(*build));
   ex->cards = NULL;
   ex->n_cards = 0;
   for (int i = 0 ; i < 52;i++){
     card_t add = card_from_num(i);
-    for (int j = 0 , j < n_hands ; j++){
+    for (int j = 0 ; j < n_hands ; j++){
     if (!deck_contains(hands[j],add)){
       add_card_to(build,add);
     }
