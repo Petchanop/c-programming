36d68b0a75a6d135bef76caf08da6818089703d0
Coursera Learner <nobody@nowhere.nul>
2021-03-07 17:22:15 +0000

    Done

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -5,7 +5,7 @@
 
 void add_card_to(deck_t * deck, card_t c){
   deck->cards = realloc(deck->cards,(deck->n_cards+1)*sizeof(*deck->cards));
-  deck->cards[deck->n_cards] = c;
+  deck->cards[deck->n_cards] = *c;
 
 }
 
