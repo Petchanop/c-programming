99e3212e6328c94cd65ccb6863c6c44166c1547f
Coursera Learner <nobody@nowhere.nul>
2021-03-07 16:40:04 +0000

    Done

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -21,7 +21,7 @@ deck_t * make_deck_exclude(deck_t * excluded_cards){
   deck_t * ex = malloc(sizeof(*ex));
 for (int i = 0 ; i < 52;i++){
   card_t add = card_from_num(i);
-  if (deck_contains(excluded_cards,add)){
+  if (!deck_contains(excluded_cards,add)){
      add_card_to(ex,add);
   }
  }
