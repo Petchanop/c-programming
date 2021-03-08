22f3c343b705234842be30acb915cf0e909c6d65
Coursera Learner <nobody@nowhere.nul>
2021-03-08 21:01:07 +0000

    Done

---
+++ c4prj2_input/future.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "deck.h"
+#include "future.h"
+#include "cards.h"
+
+void add_future_card(future_cards_t * fc, size_t index, card_t * ptr){
+  fc->decks = realloc(fc->decks,(fc->n_decks+1)*sizeof(*fc->decks));
+  ptr += index;
+  fc->decks[fc->n_decks]->cards[fc->decks[fc->n_decks]->n_cards] = ptr;
+ 
+}
+
+void future_cards_from_deck(deck_t * deck, future_cards_t * fc){
+  for ( int i = 0 ; i < fc->n_decks ; i++){
+     for (int j = deck->n_cards ; j > fc->decks[i]->n_cards ; j--){
+       add_future_card(fc,i,deck->cards[j]); 
+    }
+  }
+}
+
---
+++ c4prj2_input/input.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "deck.h"
+#include "eval.h"
+#include "input.h"
+#include "future.h"
+
+deck_t * hand_from_string(const char * str, future_cards_t * fc){
+  deck_t * hand;
+  fc = realloc(fc,(fc->n_decks+1)*sizeof(*fc));
+  char * copy = malloc(strlen(str)*sizeof(copy));
+  strcpy(copy,str);
+  char * card = strtok(copy," ");
+  while ( card != NULL){  
+    card_t c = card_from_letter(copy[0],copy[1]);
+    if( c->value != "?"){
+      add_card_to(fc->deck[n_decks],c);
+    }
+    else{
+      card_t * unknown = add_empty_card(hand);
+     add_future_card(fc,fc->n_decks[n_decks]->cards[deck->n_cards],unknown);
+    }
+    card = strtok(NULL," ");
+}
+  fc->n_decks++;
+  return fc->deck;
+}
+
+deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc){
+  char * input = NULL;
+  deck_t ** read = malloc(n_hands*sizeof(*read));
+  size_t sz = 0;
+  int i = 0;
+  while (getline(&input,&sz, f) >= 0) {
+    read[i] = hand_from_string(input,fc);
+    i++;
+   }
+  read->n_cards = i;
+   free(input);
+return read;
+}
