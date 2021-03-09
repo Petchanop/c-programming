1d4cefa6cb95fa4900df14404f494a087d883c2a
Coursera Learner <nobody@nowhere.nul>
2021-03-09 10:57:58 +0000

    Done

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -14,9 +14,11 @@ void add_future_card(future_cards_t * fc, size_t index, card_t * ptr){
 }
 
 void future_cards_from_deck(deck_t * deck, future_cards_t * fc){
+  
   for ( int i = 0 ; i < fc->n_decks ; i++){
-     for (int j = deck->n_cards ; j > fc->decks[i].n_cards ; j--){
-       add_future_card(fc,i,deck->cards[j]); 
+     for (int j = 0 ; j > fc->decks[i].n_cards ; j--){
+       (*(fc->decks[i].cards[j])).value = (*(deck->cards[i])).value;
+       (*(fc->decks[i].cards[j])).suit = (*(deck->cards[i])).suit;
     }
   }
 }
