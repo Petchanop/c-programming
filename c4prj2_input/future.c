32a355e6d8d76c4dcfbaa2b4d60afc23f76d25ee
Coursera Learner <nobody@nowhere.nul>
2021-03-09 11:05:33 +0000

    Done

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -14,7 +14,10 @@ void add_future_card(future_cards_t * fc, size_t index, card_t * ptr){
 }
 
 void future_cards_from_deck(deck_t * deck, future_cards_t * fc){
-  
+  if (deck->n_cards< fc->n_decks){
+    fprintf(stderr,"future_cards_from_deck");
+    return;
+  }
   for ( int i = 0 ; i < fc->n_decks ; i++){
     if(fc->decks[i].n_cards == 0) continue;
      for (int j = 0 ; j < fc->decks[i].n_cards ; j--){
