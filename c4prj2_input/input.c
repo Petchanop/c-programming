616d098424f2bc33d4ee922fd86a95cb59631de8
Coursera Learner <nobody@nowhere.nul>
2021-03-09 11:15:39 +0000

    Done

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -37,7 +37,7 @@ deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc){
   size_t i = 0;
   while (getline(&input,&sz, f) >= 0) {
     read = realloc(read,(i+1)*sizeof(*read));
-    deck * t add = hand_from_string(input,fc);
+    deck_t * add = hand_from_string(input,fc);
     if (add == NULL) continue;
     read[i] = add;
     i++;
