21d7107fd1fabaa5d27b4d2b43c5fa6c53df5c1d
Petchanop <nop.hide@gmail.com>
2021-03-10 22:25:24 +0000

    Done

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -21,7 +21,7 @@ deck_t * hand_from_string(const char * str, future_cards_t * fc){
      add_future_card(fc,i,unknown);
       }
       if ( *j  == ' '){
-	card_t c = card_from_letters((*j-2),*(j-1));
+	card_t c = card_from_letters(*(j-2),*(j-1));
       add_card_to(hand,c);
       i++;
        }
