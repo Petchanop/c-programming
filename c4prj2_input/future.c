b9987bff8f1f055f44b14a19b3fb3f54798331e7
Coursera Learner <nobody@nowhere.nul>
2021-03-10 09:25:07 +0000

    Done

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -7,10 +7,21 @@
 #include "cards.h"
 
 void add_future_card(future_cards_t * fc, size_t index, card_t * ptr){
-  fc->decks = realloc(fc->decks,(fc->n_decks+1)*sizeof(*fc->decks));
-  ptr += index;
+  if(index < fc->n_decks){
+  fc->decks[index].cards = realloc(fc->decks[index].cards,(fc->n_decks[index].n_cards+1)*sizeof(*fc->decks[index].cards));
   fc->decks[index].cards[fc->decks[index].n_cards]=ptr;
   fc->decks[index].n_cards ++; 
+}
+  else{
+    while(fc->n_decks <= index){
+      fc->decks=realloc(fc->decks,(fc->n_decks+1)*sizeof(*fc->decks));
+      add_empty_cards(fc->decks[fc->n_decks].cards);
+      fc->n_decks++;
+    }
+    fc->decks[index].cards = realloc(fc->decks[index].cards,(fc->n_decks[index].n_cards+1)*sizeof(*fc->decks[index].cards));
+    fc->decks[index].cards[fc->decks[index].n_cards]=ptr;
+    fc->decks[index].n_cards ++;
+  }
 }
 
 void future_cards_from_deck(deck_t * deck, future_cards_t * fc){
