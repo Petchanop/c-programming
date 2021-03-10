fff91e1c2285120a2982bdc97af979c9ef33cdd7
Petchanop <nop.hide@gmail.com>
2021-03-10 17:25:56 +0000

    Done

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -39,10 +39,12 @@ deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc){
   while (getline(&input,&sz, f) >= 0) {
     read = realloc(read,(i+1)*sizeof(deck_t*));
     deck_t * add = hand_from_string(input,fc);
+    print_hand(add);
     read[i] = add;
     i++;
     }
-   free(input);
+    free(input);
    *n_hands = i;
+   
 return read;
 }
