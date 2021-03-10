172bd33ed3afce95bcc8d37d094f4a9ba5a9b7ad
Coursera Learner <nobody@nowhere.nul>
2021-03-10 09:28:45 +0000

    Done

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -8,17 +8,17 @@
 
 void add_future_card(future_cards_t * fc, size_t index, card_t * ptr){
   if(index < fc->n_decks){
-  fc->decks[index].cards = realloc(fc->decks[index].cards,(fc->n_decks[index].n_cards+1)*sizeof(*fc->decks[index].cards));
+  fc->decks[index].cards = realloc(fc->decks[index].cards,(fc->decks[index].n_cards+1)*sizeof(*fc->decks[index].cards));
   fc->decks[index].cards[fc->decks[index].n_cards]=ptr;
   fc->decks[index].n_cards ++; 
 }
   else{
     while(fc->n_decks <= index){
       fc->decks=realloc(fc->decks,(fc->n_decks+1)*sizeof(*fc->decks));
-      add_empty_cards(fc->decks[fc->n_decks].cards);
+      add_empty_card(fc->decks[fc->n_decks].cards);
       fc->n_decks++;
     }
-    fc->decks[index].cards = realloc(fc->decks[index].cards,(fc->n_decks[index].n_cards+1)*sizeof(*fc->decks[index].cards));
+    fc->decks[index].cards = realloc(fc->decks[index].cards,(fc->decks[index].n_cards+1)*sizeof(*fc->decks[index].cards));
     fc->decks[index].cards[fc->decks[index].n_cards]=ptr;
     fc->decks[index].n_cards ++;
   }
