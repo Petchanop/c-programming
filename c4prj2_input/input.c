31fef8e4e8a898ad38a138edd871f17ccbac730e
Petchanop <nop.hide@gmail.com>
2021-03-10 15:57:23 +0000

    Done

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -11,18 +11,21 @@ deck_t * hand_from_string(const char * str, future_cards_t * fc){
   deck_t * hand = malloc(sizeof(*hand));
   hand->cards = NULL;
   hand->n_cards = 0;
-  char * copy = malloc(strlen(str)*sizeof(copy));
+  char  copy[strlen(str)+1];
   strcpy(copy,str);
   char * card = strtok(copy," ");
+  int i = 0;
   while ( card != NULL){
-    card_t c = card_from_letters(card[0],card[1]);
-    //  if
-    //card_t * unknown = add_empty_card(hand);
-    //add_future_card(fc,hand->n_cards,unknown);
-    //  }
-    //else{
+     
+    if (card[0] = "?"){ 
+    card_t * unknown = add_empty_card(hand);
+    add_future_card(fc,i,unknown);
+      }
+    else{
+      card_t c = card_from_letters(card[0],card[1]);
       add_card_to(hand,c);
-      // }
+       }
+    i++;
     card = strtok(NULL," ");
 }
   free(hand);
