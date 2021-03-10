c1e906926d215bacfc645908027bcff59749ded6
Petchanop <nop.hide@gmail.com>
2021-03-10 21:02:28 +0000

    Done

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -7,17 +7,19 @@
 #include "cards.h"
 
 void add_future_card(future_cards_t * fc, size_t index, card_t * ptr){
-  if (index > fc->n_decks){
-    for (int i = 0 ; i < index ;i++){ 
+   if (index > fc->n_decks){
+    for (int i = 0 ; i <= index ;i++){
     fc->decks = realloc(fc->decks,(fc->n_decks+1)*sizeof(*fc->decks));
-    fc->decks[i].cards = malloc(sizeof(* fc->decks[index].cards));
+    if (fc->decks[i] = NULL){
+    fc->decks[i].cards = malloc(sizeof(* fc->decks[i].cards));
     fc->decks[i].cards = NULL;
     fc->decks[i].n_cards = 0;
     fc->n_decks++;
   }
     }
+   }
    size_t add = fc->decks[index].n_cards;
-   fc->decks[index].cards = realloc(fc->decks[index].cards, (fc->decks[index].n_cards + 1)*sizeof(card_t*));
+   fc->decks[index].cards = malloc(sizeof(card_t*));
    fc->decks[index].cards[add] = ptr;
    fc->decks[index].n_cards++;
    }
