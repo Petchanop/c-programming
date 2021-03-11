723f05272c8f08252b637cb64172b4ea550981e8
Petchanop <nop.hide@gmail.com>
2021-03-11 15:51:13 +0000

    Done

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -20,7 +20,7 @@ void add_future_card(future_cards_t * fc, size_t index, card_t * ptr){
       fc->n_decks++;
     }
     fc->decks[index].cards=realloc(fc->decks[index].cards,(fc->decks[index].n_cards+1)*sizeof(*(fc->decks[index].cards)));
-    fc->decks[index].cards[fc->decks[index].n_cards-1]= ptr;
+    fc->decks[index].cards[fc->decks[index].n_cards]= ptr;
     fc->decks[index].n_cards++;
   }
 }
@@ -40,6 +40,7 @@ void future_cards_from_deck(deck_t * deck, future_cards_t * fc){
     for (int j = 0 ; j < fc->decks[i].n_cards ; j++){
       card_t * c = deck->cards[j];
       fc->decks[i].cards[j] = c;
+      deck->n_cards--;
      
     }
   }
